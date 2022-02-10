40994616d197256b722160f62b2f731d6410de59
manoj_kumar57 <manoj_kumar57@dell.com>
2022-02-10 13:23:13 +0530

    Init drafted changes admin portal

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule, NgZone } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, `/mf-admin-portal-root/i18n/`, '.json');
+}
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader,
+          deps: [HttpClient],
+      },
+    })
+  ],
+  exports: [
+    TranslateModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { 
+
+}
